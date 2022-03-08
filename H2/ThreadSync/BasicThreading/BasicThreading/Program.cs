c9ea7b2660963be4f7b363b61995ca1d1c4a4fe4
SnitzelKlaus <SnitzelKlaus@gmail.com>
2022-03-08 13:33:55 +0100

    Sync assignment
    Sync

---
+++ H2/ThreadSync/BasicThreading/BasicThreading/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.IO;
+using System.Diagnostics;
+namespace ThreadPooling
+{
+    class Program
+    {
+        static int sum = 0;
+
+        static void Main(string[] args)
+        {
+            Thread inc = new Thread(Increase);
+            Thread dec = new Thread(Decrease);
+
+            inc.Start();
+            dec.Start();
+
+            Console.WriteLine(sum);
+        }
+        static void Increase()
+        {
+            Interlocked.Increment(ref sum);
+            Thread.Sleep(1000);
+        }
+
+        static void Decrease()
+        {
+            Interlocked.Increment(ref sum);
+            Thread.Sleep(1000);
+        }
+    }
+}
