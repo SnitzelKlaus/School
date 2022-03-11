55648ded4334bbf35d988926b112894ab6f3d89c
SnitzelKlaus <SnitzelKlaus@gmail.com>
2022-03-11 09:25:21 +0100

    Bagage
    Bagage

---
+++ H2/BagageSortingSystem/BagageSortingSystem/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading;
+
+namespace BagageSortingSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
