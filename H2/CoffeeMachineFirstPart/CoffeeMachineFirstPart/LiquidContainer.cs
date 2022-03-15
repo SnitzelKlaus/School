d06efa87f071e201f217d4999b7bb86be1a49469
SnitzelKlaus <SnitzelKlaus@gmail.com>
2022-03-15 18:32:54 +0100

    gg
    gg

---
+++ H2/CoffeeMachineFirstPart/CoffeeMachineFirstPart/LiquidContainer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeMachineFirstPart
+{
+    public class LiquidContainer
+    {
+        private string _name;
+        private int _amount;
+
+        public string Name { get => _name; set => _name = value; }
+        public int Amount { get => _amount; set => _amount = value; }
+
+        public LiquidContainer(string name, int amount)
+        {
+            this._name = name;
+            this._amount = amount;
+        }
+    }
+}
---
+++ H2/CoffeeMachineFirstPart/CoffeeMachineFirstPart/Machine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeMachineFirstPart
+{
+    public abstract class Machine
+    {
+        private string _name;
+        private bool _isAlive;
+        private ContentContainer _content;
+        private LiquidContainer _liquid;
+
+        public string Name { get => _name; set => _name = value; }
+        public bool IsAlive { get => _isAlive; set => _isAlive = value; }
+        public ContentContainer Content { get => _content; set => _content = value; }
+
+
+        public Machine(string name)
+        {
+            this._name = name;
+        }
+    }
+}
