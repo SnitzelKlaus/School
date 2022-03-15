82ca9d177f96862fba9ff73b6e151df02e3c5345
SnitzelKlaus <SnitzelKlaus@gmail.com>
2022-03-15 20:30:27 +0100

    GG
    GG

---
+++ H2/CoffeeMachineFirstPart/CoffeeMachineFirstPart/Container.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeMachineFirstPart
+{
+    public abstract class Container
+    {
+        private string _item;
+        private int _currentAmount;
+        private int _maxAmount;
+
+        public string Item { get => _item; set => _item = value; }
+        public int Amount { get => _currentAmount; set => _currentAmount = value; }
+        public int MaxAmount { get => _maxAmount; set => _maxAmount = value; }
+
+        public Container(string item, int amount, int maxAmount)
+        {
+            _item = item;
+            _currentAmount = amount;
+            _maxAmount = maxAmount;
+        }
+
+        public virtual void IncreaseItem(int input)
+        {
+            if(input < MaxAmount)
+            {
+                Amount = Amount + input;
+            }
+            else
+            {
+                MaxAmount = Amount;
+                Console.WriteLine("You've filled the container to the top!");
+            }
+        }
+        public virtual void DecreaseItem(int input)
+        {
+            if(input > Amount)
+            {
+                Amount = Amount - input;
+            }
+            else
+            {
+                Console.WriteLine("The container is running low, please fill it up and try again!");
+            }
+        }
+    }
+}
---
+++ H2/CoffeeMachineFirstPart/CoffeeMachineFirstPart/Liquid.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeMachineFirstPart
+{
+    public class Liquid : Container
+    {
+        public Liquid(string item, int amount, int maxAmount):base(item, amount, maxAmount)
+        {
+
+        }
+    }
+}
