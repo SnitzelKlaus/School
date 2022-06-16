4693eea17d815f9101d62fee8953e86ed790fb51
SnitzelKlaus <SnitzelKlaus@gmail.com>
2022-06-16 12:40:43 +0200

    gg
    gg

---
+++ H2/DatabaseFirst/DatabaseFirst/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFirst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (LufthavnDbEntities context = new LufthavnDbEntities())
+            {
+                context.spCompanyAndRoute_Create("MIST");
+
+                context.spAirport_Create("TESA", "ZBC Cool", "Ringsted ZBC 31A");
+                context.spPlane_Create("69420DK", 1, 1, "TES");
+
+                context.SaveChanges();
+            };
+        }
+    }
+}
