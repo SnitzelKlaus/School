38a7fc5e80619e4135ea1c343c150b9a27ec07ca
SnitzelKlaus <SnitzelKlaus@gmail.com>
2022-06-22 12:13:53 +0200

    gg
    gg

---
+++ H2/Kreditkortet/Kreditkortet/Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kreditkortet
+{
+    public class Card
+    {
+        public string Number { get => _number; set => _number = value; }
+        public bool IsValid { get => _isValid; set => _isValid = value; }
+
+
+        private string _number;
+        private bool _isValid;
+
+        public Card(string number)
+        {
+            Number = number;
+        }
+    }
+}
---
+++ H2/Kreditkortet/Kreditkortet/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kreditkortet
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const char Separator = ',';
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void cardReader_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            List<string> cards = new List<string>();
+
+            cards.AddRange(cardReader.Text.Split(Separator));
+
+            foreach (var card in cards)
+            {
+
+            }
+        }
+    }
+}
