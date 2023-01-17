28ff64949e344bda48622eb899d327f0f45c4a52
SnitzelKlaus <73334542+SnitzelKlaus@users.noreply.github.com>
2023-01-17 20:18:37 +0100

    ConsoleBrowser
    ConsoleBrowser

---
+++ H3/Serverside/ConsoleBrowser/ConsoleBrowser/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleBrowser
+{
+    class Program
+    {
+        HttpClient client = new HttpClient();
+
+        static async Task Main(string[] args)
+        {
+            Program program = new Program();
+            await program.GetPage("https://www.google.com/finance/");
+        }
+
+        private async Task GetPage(string uri)
+        {
+            string data = await client.GetStringAsync(uri);
+            Console.WriteLine(data);
+        }
+    }
+}
