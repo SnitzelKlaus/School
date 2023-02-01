80f130d7d537b2657ab55f0e17d92900d594cca6
SnitzelKlaus <73334542+SnitzelKlaus@users.noreply.github.com>
2023-02-01 21:53:31 +0100

    gg
    gg

---
+++ H3/testscrumboard/lib/main.dart
@@ -0,0 +1,172 @@
+import 'package:drag_and_drop_lists/drag_and_drop_item.dart';
+import 'package:drag_and_drop_lists/drag_and_drop_list.dart';
+import 'package:drag_and_drop_lists/drag_and_drop_lists.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Scrum Board Demo',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late List<DragAndDropList> _contents;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _contents = List.generate(3, (index) {
+      return DragAndDropList(
+        header: Column(
+          children: <Widget>[
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 8, bottom: 4),
+                  child: Text(
+                    'Task: $index',
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        footer: OutlinedButton(
+          onPressed: () {},
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.white),
+          ),
+          child: const Text(
+            'Add',
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+        children: <DragAndDropItem>[
+          DragAndDropItem(
+            child: Row(
+              children: [
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+                  child: Text(
+                    'Test $index.1',
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var backgroundColor = Colors.blueGrey[900];
+
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      appBar: AppBar(
+          title: const Text('Scrum Board'),
+          backgroundColor: Colors.blueGrey[800]),
+      body: DragAndDropLists(
+        axis: Axis.horizontal,
+        listWidth: 300,
+        children: _contents,
+        onItemReorder: _onItemReorder,
+        onListReorder: _onListReorder,
+        listPadding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+        itemDivider: Divider(
+          thickness: 2,
+          height: 2,
+          color: backgroundColor,
+        ),
+        itemDecorationWhileDragging: BoxDecoration(
+          color: Colors.greenAccent,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 2,
+              blurRadius: 3,
+              offset: const Offset(0, 0), // changes position of shadow
+            ),
+          ],
+        ),
+        listInnerDecoration: BoxDecoration(
+          color: Theme.of(context).canvasColor,
+          borderRadius: const BorderRadius.all(Radius.circular(8.0)),
+        ),
+        lastItemTargetHeight: 8,
+        addLastItemTargetHeightToTop: true,
+        lastListTargetSize: 40,
+        listDragHandle: const DragHandle(
+          verticalAlignment: DragHandleVerticalAlignment.top,
+          child: Padding(
+            padding: EdgeInsets.only(right: 10),
+            child: Icon(
+              Icons.moped_rounded,
+              size: 45,
+              color: Color.fromARGB(255, 48, 144, 255),
+            ),
+          ),
+        ),
+        itemDragHandle: const DragHandle(
+          child: Padding(
+            padding: EdgeInsets.only(right: 10),
+            child: Icon(
+              Icons.menu,
+              color: Colors.blueGrey,
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.blueGrey[800],
+        child: const Icon(
+          Icons.add,
+          size: 40,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+
+  _onItemReorder(
+      int oldItemIndex, int oldListIndex, int newItemIndex, int newListIndex) {
+    setState(() {
+      var movedItem = _contents[oldListIndex].children.removeAt(oldItemIndex);
+      _contents[newListIndex].children.insert(newItemIndex, movedItem);
+    });
+  }
+
+  _onListReorder(int oldListIndex, int newListIndex) {
+    setState(() {
+      var movedList = _contents.removeAt(oldListIndex);
+      _contents.insert(newListIndex, movedList);
+    });
+  }
+}
