cb2e8fb8fc93cb2db686efd8063c058c28f7d258
Benjamin Navnebeskyttet (BENJ4651) <benj4651@zbc.dk>
2023-03-18 14:42:25 +0100

    gg
    gg

--- H3/Angular/fosil-cars/src/app/app-routing.module.ts
+++ H3/Angular/fosil-cars/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarIdComponent } from './components/car-id/car-id.component';
+import { CarComponent } from './components/car/car.component';
 
 const routes: Routes = [
-  
+  { path: 'car', component: CarComponent },
+  { path: 'car/:id', component: CarIdComponent },
+  { path: '', redirectTo: '/car', pathMatch: 'full' }
 ];
 
 @NgModule({
---
+++ H3/Angular/node.js/rest-api/app.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+jsonArray = require('./stocks.json');
+jsonArray = jsonArray['stocks'];
+const array = [];
+jsonArray.forEach(element => {
+    array.push(element)
+});
+
+app.get('/', (req, res) => {
+    res.send('Node.js server is running')
+});
+
+app.get('/stocks', (req, res) => {
+    res.send(array)
+});
+
+app.post('/add/stock', (req, res) => {
+    const name = req.body.name;
+    const symbol = req.body.symbol;
+    const price = req.body.price;
+
+    const newStock = {
+        name: name,
+        symbol: symbol,
+        price: price
+    }
+
+    array.push(newStock);
+    res.send(array);
+});
+
+app.delete('/delete/stock/:symbol', (req, res) => {
+    // Get the symbol from the request parameters.
+    const symbol = req.params.symbol;
+
+    // Find the index of the stock in the array based on its symbol.
+    const index = array.findIndex((stock) => stock.symbol === symbol);
+
+    // If the stock exists in the array, remove it from the array.
+    if (index !== -1) {
+        array.splice(index, 1);
+    }
+
+    // Send the modified array as the response.
+    res.send(array);
+});
+
+app.put('/update/stock/:symbol', (req, res) => {
+    // The request parameter 'symbol' is assigned to a constant 'symbol'.
+    const symbol = req.params.symbol;
+
+    // Gets the index of the stock in the array based on its symbol.
+    const index = array.findIndex((stock) => stock.symbol === symbol);
+
+    // If the stock exists in the array, update its name, symbol and price.
+    if (index !== -1) {
+        array[index].name = req.body.name;
+        array[index].symbol = req.body.symbol;
+        array[index].price = req.body.price;
+    }
+
+    // Send the modified array as the response.
+    res.send(array);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
