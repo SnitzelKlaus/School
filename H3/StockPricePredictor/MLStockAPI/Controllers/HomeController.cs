4b51697720a97ee90a6d306e2636eb05e0b422b5
snitzel_klaus <SnitzelKlaus@gmail.com>
2023-06-09 08:14:22 +0200

    gg
    gg

---
+++ H3/StockPricePredictor/MLStockAPI/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MLStockAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
